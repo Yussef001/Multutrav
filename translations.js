c030819b6d74660e06c74c24a36c3360c16e83fb
Yussef001 <abdelyussefs@gmail.com>
2024-12-14 10:38:03 +0000

    ajout de la barre de traduction

---
+++ translations.js
@@ -0,0 +1,14 @@
+const translations = {
+    fr: {
+        navHome: "Accueil",
+        navAbout: "À propos de nous",
+        navServices: "Nos services",
+        navContact: "Contact",
+    },
+    en: {
+        navHome: "Home",
+        navAbout: "About Us",
+        navServices: "Our Services",
+        navContact: "Contact",
+    },
+};
